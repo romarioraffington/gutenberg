f99a009fca640b9732a645c75c8eb6d55059ae74
Jarda Snajdr <jsnajdr@gmail.com>
2021-08-27 09:17:02 +0200

    Fixup dispatch calls and ifNotResolved waiting

--- packages/core-data/src/actions.js
+++ packages/core-data/src/actions.js
@@ -660,9 +660,7 @@ export const __experimentalSaveSpecifiedEntityEdits = (
 			editsToSave[ edit ] = edits[ edit ];
 		}
 	}
-	return await dispatch(
-		saveEntityRecord( kind, name, editsToSave, options )
-	);
+	return await dispatch.saveEntityRecord( kind, name, editsToSave, options );
 };
 
 /**

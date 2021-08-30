96ba93b9ee1e3daf19746af824ee8888ec5456f0
Adam Zieliński <adam@adamziel.com>
2021-08-30 16:35:47 +0200

    Add rawBlockMarkupFields

--- packages/core-data/src/actions.js
+++ packages/core-data/src/actions.js
@@ -373,6 +373,7 @@ export const saveEntityRecord = (
 	}
 	const entityIdKey = entity.key || DEFAULT_ENTITY_KEY;
 	const recordId = record[ entityIdKey ];
+	const rawBlockMarkupFields = entity.rawBlockMarkupFields || [];
 
 	const lock = await dispatch(
 		__unstableAcquireStoreLock(
@@ -440,9 +441,7 @@ export const saveEntityRecord = (
 				let data = { ...persistedRecord, ...autosavePost, ...record };
 				data = Object.keys( data ).reduce(
 					( acc, key ) => {
-						if (
-							[ 'title', 'excerpt', 'content' ].includes( key )
-						) {
+						if ( rawBlockMarkupFields.includes( key ) ) {
 							// Edits should be the "raw" attribute values.
 							acc[ key ] = get( data[ key ], 'raw', data[ key ] );
 						}
@@ -474,11 +473,7 @@ export const saveEntityRecord = (
 					newRecord = Object.keys( newRecord ).reduce(
 						( acc, key ) => {
 							// These properties are persisted in autosaves.
-							if (
-								[ 'title', 'excerpt', 'content' ].includes(
-									key
-								)
-							) {
+							if ( rawBlockMarkupFields.includes( key ) ) {
 								// Edits should be the "raw" attribute values.
 								acc[ key ] = get(
 									newRecord[ key ],
